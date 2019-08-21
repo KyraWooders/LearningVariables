ceabffbc495be757e00f02f0704b25cc0c5323f3
s198061 <s198061@LAF-7E868C>
2019-08-21 09:03:55 -0500

    Add project files.

---
+++ LearningVariables/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningVariables
+{
+    class Program
+    {
+        static void Main()
+        {
+            //the value
+            string greeting = "Hello, user!";
+            int playerScore = 0;
+            string displayScore = "Your score currently is " + playerScore + ". ";
+            //to start the output
+            Console.WriteLine(greeting);
+            Console.WriteLine(displayScore);
+            Console.ReadKey();
+        }
+    }
+}
